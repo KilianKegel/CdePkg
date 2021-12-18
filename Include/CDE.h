509150ad5387ef247357744416354ab2f5c42263
KilianKegel <kilian_kegel@hotmail.com>
2021-12-18 14:04:12 +0100

    remove main(), wmain() defaults from the library

--- Include/CDE.h
+++ Include/CDE.h
@@ -1,14 +1,24 @@
-/*!
-@copyright
-    Copyright (c) 2020, Kilian Kegel. All rights reserved.<BR>
-    SPDX-License-Identifier: BSD-2-Clause-Patent
+/*++
 
-@file
-    CDE.h
-@brief
-    C Development Environment (CDE) base definitions
-@todo
-*/
+    toro C Library
+    https://github.com/KilianKegel/toro-C-Library#toro-c-library-formerly-known-as-torito-c-library
+
+    Copyright (c) 2017-2021, Kilian Kegel. All rights reserved.
+    SPDX-License-Identifier: GNU General Public License v3.0
+
+Module Name:
+
+    Cde.h
+
+Abstract:
+
+    C Development Environment base definitions
+
+Author:
+
+    Kilian Kegel
+
+--*/
 #ifndef _CDE_H_
 #define _CDE_H_
 #include <stdio.h>
